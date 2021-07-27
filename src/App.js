a8b2e6121780ec6779bd0e7491a064ed2f9c51f1
ericmbouwe <ericmbouwe@yahoo.fr>
2021-07-27 19:34:43 +0100

    add social icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import MainSide from './components/MainSide';
 import ScrollSide from './components/ScrollSide';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faCheckSquare, faCoffee);
 
 function App() {
   return (
--- src/components/MainSide.js
+++ src/components/MainSide.js
@@ -1,80 +1,80 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function MainSide() {
-    return (
-        <main>
-            <div className="about-content">
-                <h1 className="title font-weight-bold">
-                    Full-Stack
-            <br />
-            Developer. <br />
-            Passionate
-            <br />
-            &amp; Creative.
-          </h1>
-                <p className="description">
-                    Hi! My name is{' '}
-                    <span className="keyword">
-                        <b>@Eric Mbouwe</b>
-                    </span>
-            , I am passionate about new technologies and business growth.
-            Graduate in economics, I am a problem solver and my favorite{' '}
-                    <span className="keyword">
-                        <b>@stack</b>
-                    </span>{' '}
-            is{' '}
-                    <span className="keyword">
-                        <b>React</b>
-                    </span>
-                    <span className="keyword"> | </span>
-                    <span className="keyword">
-                        <b>Ruby on Rails</b>
-                    </span>
-            . I am a clean code enthusiastic and a good team player.
-            <br />
-            Follow my work below
-          </p>
-                <div className="social-links">
-                    <ul className="d-flex justify-content-start">
-                        <li>
-                            <a
-                                href="https://www.linkedin.com/in/ericmbouwe/"
-                                target="blank"
-                            >
-                                <i className="fa fa-linkedin" aria-hidden="true" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/EricMbouwe" target="blank">
-                                <i className="fa fa-github" aria-hidden="true" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/EricMbouwe" target="blank">
-                                <i className="fa fa-twitter" aria-hidden="true" />
-                            </a>
-                        </li>
-                        <li className="file">
-                            <a
-                                href="assets/images/RESUME-ERIC-EN.pdf"
-                                data-toggle="tooltip"
-                                data-placement="top"
-                                title="Download resume"
-                                download
-                            >
-                                <i className="fa fa-file-text" aria-hidden="true" />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <a
-                    className="hello-btn d-block"
-                    href="mailto:ericmbouwe@gmail.com"
-                    target="blank"
-                >
-                    Get in Touch!
-                </a>
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <div className="about-content">
+        <h1 className="title font-weight-bold">
+          Full-Stack
+          <br />
+          Developer. <br />
+          Passionate
+          <br />
+          &amp; Creative.
+        </h1>
+        <p className="description">
+          Hi! My name is{' '}
+          <span className="keyword">
+            <b>@Eric Mbouwe</b>
+          </span>
+          , I am passionate about new technologies and business growth. Graduate
+          in economics, I am a problem solver and my favorite{' '}
+          <span className="keyword">
+            <b>@stack</b>
+          </span>{' '}
+          is{' '}
+          <span className="keyword">
+            <b>React</b>
+          </span>
+          <span className="keyword"> | </span>
+          <span className="keyword">
+            <b>Ruby on Rails</b>
+          </span>
+          . I am a clean code enthusiastic and a good team player.
+          <br />
+          Follow my work below
+        </p>
+        <div className="social-links">
+          <ul className="d-flex justify-content-start">
+            <li>
+              <a href="https://www.linkedin.com/in/ericmbouwe/" target="blank">
+                <FontAwesomeIcon icon={['fab', 'linkedin']} />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/EricMbouwe" target="blank">
+                <FontAwesomeIcon icon={['fab', 'github']} />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/EricMbouwe" target="blank">
+                <FontAwesomeIcon icon={['fab', 'twitter']} />
+              </a>
+            </li>
+            <li className="file">
+              <a
+                href="assets/images/RESUME-ERIC-EN.pdf"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="Download resume"
+                download
+              >
+                {/* <FontAwesomeIcon icon={['fab', 'file']} /> */}
+                {/* <i className="fa fa-file-text" aria-hidden="true" /> */}
+              </a>
+            </li>
+          </ul>
+        </div>
+        <a
+          className="hello-btn d-block"
+          href="mailto:ericmbouwe@gmail.com"
+          target="blank"
+        >
+          Get in Touch!
+        </a>
+      </div>
+    </main>
+  );
 }
 
-export default MainSide
+export default MainSide;
