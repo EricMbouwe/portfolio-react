1e5ffac699fb43344ee6fd4565709333abdc3ba2
ericmbouwe <ericmbouwe@yahoo.fr>
2021-11-22 20:52:51 +0100

    add link prop to article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,11 @@
 function Article({ data }) {
-  const { title, date } = data;
+  const { title, date, link } = data;
 
   return (
     <div>
       <small className="article_date d-block">{date}</small>
       <a
-        href="https://hackernoon.com/css-basics-how-to-take-a-control-gm6k3vxd"
+        href={link}
         className="article_title text-black"
         target="blank"
       >
