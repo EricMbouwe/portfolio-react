8298c02b7bd7b83ba2459f665d256dfe2b917a6c
ericmbouwe <ericmbouwe@yahoo.fr>
2022-01-15 18:41:36 +0100

    Update the article id again

--- src/utils/articles.js
+++ src/utils/articles.js
@@ -1,6 +1,6 @@
 const articles = [
   {
-    id: 2,
+    id: 1,
     title: 'CSS Basics: How To Take A Control',
     link: 'https://hackernoon.com/css-basics-how-to-take-a-control-gm6k3vxd',
     date: '20/05/2020',
